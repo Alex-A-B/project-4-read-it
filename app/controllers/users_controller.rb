dc65a0a4a3c88116b46f60eabd79319552e49f63
Alex-A-B <alexjamesbelton@gmail.com>
2022-02-03 20:59:21 +0000

    Create User route working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,15 @@ class UsersController < ApplicationController
         end
       end
 
+    def create
+      user = User.create(user_params)
+      render json: user
+    end
+
+    private
+
+    def user_params
+      params.permit(:username, :password)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  
+    
+    has_secure_password
+
+    validates :username, uniqueness: true
+
     has_many :books
     
 end
