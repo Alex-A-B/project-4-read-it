fb0eb45e218e01a9b261111f74bca020b60c2c61
Alex-A-B <alexjamesbelton@gmail.com>
2022-02-04 08:02:33 +0000

    Fixed error with user library

--- client/src/components/UserLibrary.js
+++ client/src/components/UserLibrary.js
@@ -1,16 +1,12 @@
-
 import Book from "./Book"
-
 
 function UserLibrary( { user, books, onRemoveBook }) {
 
-
-    // needs an IF to stop the react error 
-    const renderLibrary = books.filter(book => book.user_id === user.id).map((book) => <Book key={book.id} book={book} onRemoveBook={onRemoveBook}/>)
+// if statement added to remove error 
 
     return (
         <div>
-           {renderLibrary}
+           {user ? ( books.filter(book => book.user_id === user.id).map((book) => <Book key={book.id} book={book} onRemoveBook={onRemoveBook}/>)): (<></>) }
         </div>
     )
 }
